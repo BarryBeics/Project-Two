bfb987f2522883bf8031a6529e43f696e3ebd683
Barry Marples <barrymarples@gmail.com>
2022-02-19 08:10:36 +0000

    set up js structure of application

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,3 +1,11 @@
+ // info popover functionality
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
 let inputDays = 2;
 let numberOfTrades = 4;
 let tradeDuration = 10;
---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+ // info popover functionality
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+function chooseVolatility() {
+
+}
+
+function exchangeRate(){
+
+}
+
+function takeProfit(){
+
+}
+
+function stopLoss(){
+
+}
+
+function timedOutMarketOrder() {
+
+}
+
+function generateRandonMove() {
+
+}
+
+function timeIntoTrade() {
+
+}
+
+function checkVolatility() {
+
+}
+
+function winLossAnalysis() {
+
+}
+
+function strategyReport() {
+
+}
