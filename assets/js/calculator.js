a9bf30a57909a93554101f05fb76fb06d488037f
Barry Marples <barrymarples@gmail.com>
2022-02-19 08:43:01 +0000

    Adding placeholder text and span id's

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,9 +1,4 @@
- // info popover functionality
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
 
 
 let inputDays = 2;
