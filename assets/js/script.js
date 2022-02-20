87e6e954a35fc2ed237ec57529a0d11dbb900cf8
Barry Marples <barrymarples@gmail.com>
2022-02-20 07:41:25 +0000

    Added event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+      button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "calculate") {
+              alert("You clicked calculate!");
+          } else {
+              let timeOption = this.getAttribute("data-type");
+              alert(`You clicked ${timeOption}`);
+          }
+      });
+  }
+});
+ 
+ 
  // info popover functionality
 
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
