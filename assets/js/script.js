74be684317fae5f14d67c29adc7f084037986b72
Barry Marples <barrymarples@gmail.com>
2022-02-26 19:41:38 +0000

    deleted redudent files, added text to the info buttons & add more comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,16 +89,91 @@ function calculateCorrectAnswer() {
   return [stake + takeProfit + stopLoss + avgTrueRange + marketMommentum + tradingFee + tradeDuration + days ];
 }
 
+
+
+
+
+
+
+  /************************************************************************************************************************************************************************************************************************************************
+ *      TPC TRADE PROBABILTY CACULATOR
+ * 
+ * This application allows the user to experiment with the parameters to gain a better understadning of how their strategy would preform.
+ * This application then simulates the market buy changing the assets price over a given period. Then off the incremental price the
+ * application produces randomly generated moves Up and Down in order to create a realistic simulation of the market.
+ ************************************************************************************************************************************************************************************************************************************************/
+
+  /****************************************
+ *      INVESTMENT AMOUNT  & FEES
+ ****************************************/
+   let stake = 0; // 1st of 8 options the user will set the user on the calculator.html page
+
+/* For development purpose we use a static amount of 10,000 as this will aid debugging */
+/* There's potential to use an API call to get live prices */
+
+/* Exchange rate */
+ function exchangeRate(cryptoPrice) {
+ let valueHeld = stake / cryptoPrice;
+ console.log('Exchange rate:  1 FIAT:', valueHeld.toFixed(2), 'CRYPTO')
+ }
+ 
+ let tradingFee = 0; // 2nd of 8 options the user will set the user on the calculator.html page
+
+
+  /****************************************
+ *      TAKE PROFIT & STOP LOSS OPTIONS
+ ****************************************/
+
+   let takeProfit = 0; // 3rd of 8 options the user will set the user on the calculator.html page
+   let stopLoss = 0; // 4th of 8 options the user will set the user on the calculator.html page
+
+
+
+  /****************************************
+ *      ATR (Average True Range) & MARKET MOMENTUM -- TRADE ENTRY CONDITIONS
+ ****************************************/
+  /* The condtions to enter a trade are that a coin has been trending and the uptrend gain for the period of time here input_confimation_duration
+   * This allows the user to test the condtions of when a bot would enter a trade, for example if the assets in question had gained 4% over 60 input_minutes
+   * then we would replicate this momentum with the following settings, if you looking of a 3 hour period just increase the input_confimation_duration = 180 */
+ 
+
+  
+   let avgTrueRange = 0;   // 5th of 8 options the user will set the user on the calculator.html page
+
+   let volatility = 0;
+   /* Also the ATR here will need to be relative to the validation period the market momentum, so this serves a another condtion that needs to be met before we enter a trade.
+   #this determines the how far up and down from the average gain price the actual price moves, this script bedefault generates a move of up to 0.9%
+   # Therefore if you change this to 1 the move can be upto 1.9% in either direction making a an ATR of 3.8% which im calling upto 4% */
+   /* Users can select from a drop down menu of options */
+   
+   /* THIS COULD BE SOMPLIFIED AS JUST HALFING THE VOLATILITY INPUT */
+   function chooseVolatility(){
+     
+     let volatilityOptions = {
+       2 : 1,
+       4 : 2,
+       6 : 4,
+       8 : 4,
+       10 : 5
+     };
+     volatility = volatilityOptions[avgTrueRange];
+     
+     }
+
+     let marketMommentum = 0;  // 6th of 8 options the user will set the user on the calculator.html page
+
+
+  /****************************************
+ *      PERCENATGES
+ ****************************************/
+
+
 // Define the variables as zero, then the users input can update these variables values
 // THIS MAY NOT BE THE BEST WAY, REVIEW THIS ONCE APPLICTION IS FUNCTIONAL
 let tradeOpen = true;
-let stake = 0;
-let takeProfit = 0;
-let stopLoss = 0;
-let avgTrueRange = 0;
-let marketMommentum = 0;
-let tradingFee = 0;
-let tradeDuration = 3;
+
+
+let tradeDuration = 3; // This will need to be multipled by 60 secound, but not while we are develpoing the application
 let days = 0;
 
 
@@ -111,32 +186,6 @@ let days = 0;
   minTrades = 0;
   maxTrades = 1440 / tradeDuration; 
   return Math.floor(Math.random() * (maxTrades - minTrades) + minTrades); 
-}
-
-
-let volatility = 0;
-/* Also the ATR here will need to be relative to the validation period the market momentum, so this serves a another condtion that needs to be met before we enter a trade.
-#this determines the how far up and down from the average gain price the actual price moves, this script bedefault generates a move of up to 0.9%
-# Therefore if you change this to 1 the move can be upto 1.9% in either direction making a an ATR of 3.8% which im calling upto 4% */
-/* Users can select from a drop down menu of options */
-function chooseVolatility(){
-  
-  let volatilityOptions = {
-    2 : 0,
-    4 : 1,
-    6 : 2,
-    8 : 3,
-    10 : 4
-  };
-  volatility = volatilityOptions[avgTrueRange];
-  
-  }
-
-
-/* Exchange rate */
-function exchangeRate(cryptoPrice) {
-let valueHeld = stake / cryptoPrice;
-console.log('Exchange rate:  1 FIAT:', valueHeld.toFixed(2), 'CRYPTO')
 }
 
 
@@ -185,8 +234,7 @@ function getDetails() {
 */
 function trading(){
 
-  /* For development purpose we use a static amount as this will aid debugging */
-  /* There's potential to use an API call to get live prices */
+
 exchangeRate(10000);
 
 chooseVolatility(avgTrueRange);
